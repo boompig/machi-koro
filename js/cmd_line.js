63c4b356060f74de5124342267e1d3cb81e83b05
Daniel Kats <boompigdev@gmail.com>
2015-04-19 13:35:38 -0400

    genetic algo now in place

--- js/cmd_line.js
+++ js/cmd_line.js
@@ -3,20 +3,108 @@
 // actual program starts here
 var fs = require("fs");
 var mc = require("./machi_koro_ctrl.js");
+var flags = require("./cmd_lib.js");
+var Flags = flags.Flags;
+var Darwin = require("./darwin.js").Darwin;
 
-var minReset = 5000;
 var minProgressIterations = 100;
 var maxProgressBars = 20;
+var minRemove = 10;
 
+/**
+ * Read weights from file system
+ */
 function readWeights() {
 	return JSON.parse(fs.readFileSync("./weights.js"));
 }
 
+/**
+ * Save weights to file system
+ */
 function saveWeights(weights) {
 	fs.writeFileSync("./weights.js", JSON.stringify(weights, null, 4));
 }
 
-function getController (weights, logLevel) {
+function copyWeights (weights) {
+	var copy = {};
+	for (var p in weights) {
+		copy[p] = {};
+		for (var cardName in weights[p]) {
+			copy[p][cardName] = weights[p][cardName];
+		}
+	}
+	return copy;
+}
+
+function pickPlayers (weights) {
+	// pick any 4 players from the assortment of players
+	var names = Object.keys(weights);
+	while (names.length > 4) {
+		var idx = Math.floor(Math.random() * names.length);
+		names.splice(idx, 1);
+	}
+	return names;
+}
+
+/**
+ * The question is, what does it mean to mutate an item?
+ *
+ * I will have these types of mutations
+ * 		- deletion
+ * 		- creation
+ * 		- alteration
+ *
+ * Deletion:
+ * 		- the probability that some trait gets reset to 0
+ *
+ * Creation:
+ * 		- the probability that some 0-trait gets set to blanket 500
+ *
+ * Mutation:
+ * 		- the probability that a non-zero trait is changed, but in a minor way (say +/- 100 from origin value)
+ *
+ * Return the new weights
+ */
+function mutateWeights (weights) {
+	var prMutate = 0.1;
+	var prCreate = 0.05;
+	var prDelete = 0.01;
+
+	var p, cardName, count;
+	var weightsCopy = copyWeights(weights);
+	var mFactor;
+
+	var mutateAdd = 100;
+	var creationAdd = 500;
+
+	for (p in weights) {
+		for (cardName in weights[p]) {
+			count = weights[p][count];
+			if (Math.random() < prMutate) {
+				mFactor = Math.round((Math.random() * mutateAdd * 2) - mutateAdd);
+				weightsCopy[p][cardName] = Math.max(0, weights[p][cardName] + mFactor);
+				// console.log("Mutated %s @ %d from %d to %d", cardName, p, weights[p][cardName], weightsCopy[p][cardName]);
+			} else if (count === 0 && Math.random() < prCreate) {
+				weightsCopy[p][cardName] = creationAdd;
+				// console.log("Mutated %s @ %d from %d to %d", cardName, p, weights[p][cardName], weightsCopy[p][cardName]);
+			} else if (count > 0 && Math.random() < prDelete) {
+				weightsCopy[p][cardName] = 0;
+				// console.log("Mutated %s @ %d from %d to %d", cardName, p, weights[p][cardName], weightsCopy[p][cardName]);
+			}
+		}
+	}
+
+	return weightsCopy;
+}
+
+
+
+/**
+ * Initialize the controller with the given weights
+ *
+ * @param {Array} players - Array of player names to use
+ */
+function getController (weights, logLevel, players) {
 	// command-line invocation of MachiKoro
 	var scope = {};
 	var ctrl = new mc.MachiKoroCtrl(scope);
@@ -26,6 +114,11 @@ function getController (weights, logLevel) {
 	if (logLevel) {
 		ctrl.setLogLevel(logLevel);
 	}
+
+	if (players) {
+		ctrl.playerNames = players;
+	}
+
 	ctrl.initGame();
 	return ctrl;
 }
@@ -35,8 +128,8 @@ function getController (weights, logLevel) {
  * Modify oldWeights
  * @return Number of turns in the game
  */
-function runSimul(winners, oldWeights, logLevel) {
-	var ctrl = getController(oldWeights, logLevel);
+function runSimul(winners, oldWeights, logLevel, players) {
+	var ctrl = getController(oldWeights, logLevel, players);
 
 	ctrl.playGame();
 	var wp = ctrl.getCurrentPlayer();
@@ -53,8 +146,8 @@ function runSimul(winners, oldWeights, logLevel) {
 	return ctrl.turn;
 }
 
-function initWinners() {
-	var ctrl = getController();
+function initWinners(players) {
+	var ctrl = getController(null, null, players);
 
 	var winners = {};
 
@@ -86,12 +179,6 @@ function resetWorstPlayer(winners, numIterations) {
 	saveWeights(weights);
 }
 
-/**
- * 
- * @param  {Object} winners       [description]
- * @param  {Number} avgTurns      [description]
- * @param  {Number} numIterations [description]
- */
 function printResults(winners, avgTurns, numIterations) {
 	console.log("Average number of turns per game: " + Math.round(avgTurns));
 	console.log("Winning stats:");
@@ -122,6 +209,9 @@ function computeWeightDiff (o1, o2) {
 	}
 }
 
+/**
+ * Print progress bar.
+ */
 function printProgress(iteration, maxIterations) {
 	var getNumBars = function (i) {
 		return Math.floor((i + 1) / maxIterations * maxProgressBars);
@@ -144,16 +234,76 @@ function printProgress(iteration, maxIterations) {
 	process.stdout.write("[" + bars + ">" + spaces + "]");
 }
 
-function main () {
+function getNumMutations (winners) {
+	var total = 0;
+	var players = Object.keys(winners);
+	players.sort(function (a, b) {
+		return winners[b] - winners[a];
+	});
+
+	for (var i = 0; i < players.length; i++) {
+		total += winners[players[i]];
+	}
+
+	var winningPlayer = players[0];
+	var perWin = winners[winningPlayer] / total * 100;
+	return 1 + Math.floor((perWin - 25) / 5.0);
+}
+
+function getChildName (absoluteWinner, weights) {
+	var newName, lastPart;
+	if (absoluteWinner.indexOf(" ") === -1) {
+		newName = absoluteWinner + " 2";
+	} else {
+		var lastPart = Number(absoluteWinner.split(" ")[1]);
+		lastPart++;
+		newName = absoluteWinner.split(" ")[0] + " " + String(lastPart);
+	}
+
+	while (weights.hasOwnProperty(newName)) {
+		lastPart = Number(newName.split(" ")[1]);
+		lastPart++;
+		newName = absoluteWinner.split(" ")[0] + " " + String(lastPart);
+	}
+	return newName;
+}
+
+function doMutations (winners, weights) {
+	// only absolute winner gets to mutate
+	var names = Object.keys(winners);
+	// sort names in descending order
+	names.sort(function (a, b) {
+		return winners[b] - winners[a];
+	});
+	var absoluteWinner = names[0];
+	var winnerWeights = weights[absoluteWinner];
+	var numChildren = getNumMutations(winners);
+	console.log("Mutating weights for AI %s; generated %d children", absoluteWinner, numChildren);
+
+	for (var i = 0; i < numChildren; i++) {
+		var newWeights = mutateWeights(winnerWeights);
+		var newName = getChildName(absoluteWinner, weights);
+		
+		console.log("Saving weights for new AI %s", newName);
+		weights[newName] = newWeights;
+		saveWeights(weights);
+	}
+}
+
+function oldMain () {
 	var iterations = 1;
 	var logLevel = "default";
 	if (process.argv.length > 2) {
-		iterations = process.argv[2];
+		iterations = Number(process.argv[2]);
 	}
 	if (process.argv.length > 3) {
 		switch (process.argv[3]) {
 			case "-q":
 				logLevel = "quiet";
+				console.log("Running simulation silently...");
+				break;
+			case "-qq":
+				logLevel = "silent";
 				console.log("Running simulation silently...");
 				break;
 			case "-v":
@@ -166,11 +316,17 @@ function main () {
 	}
 
 	var oldWeights = readWeights();
-	var winners = initWinners();
 	var counter = 0;
-	console.log("Running simulation for " + iterations + " iterations");
-	for (var i = 0; i < iterations; i++) {
-		counter += runSimul(winners, oldWeights, logLevel);
+
+	var players = pickPlayers(oldWeights);
+	console.log("Using these players:");
+	console.log(players);
+	var winners = initWinners(players);
+
+	console.log("Running simulation for %d iterations", iterations);
+	for (var i = 0; i < iterations; i++) {	
+		oldWeights = readWeights();
+		counter += runSimul(winners, oldWeights, logLevel, players);
 		if (iterations >= minProgressIterations && logLevel === "quiet") {
 			printProgress(i, iterations);
 		}
@@ -178,10 +334,48 @@ function main () {
 	if (iterations >= minProgressIterations && logLevel === "quiet") {
 		process.stdout.write("\n");
 	}
+	console.log("Saving computed weights");
 	printResults(winners, counter/iterations, iterations);
 
-	if (iterations >= minReset) {
-		resetWorstPlayer(winners, iterations);
+	return winners;
+}
+
+
+function main () {
+	var numMutations = 1;
+	var iterations = 1;
+	if (process.argv.length > 2) {
+		iterations = Number(process.argv[2]);
+	}
+	if (process.argv.length > 4) {
+		numMutations = Number(process.argv[4]);
+	}
+
+	for (var i = 0; i < numMutations; i++) {
+		// run the simulation
+		var winners = oldMain();
+
+		var players = Object.keys(winners);
+		// sort descending by number of wins
+		players.sort(function (a, b) {
+			return winners[b] - winners[a];
+		});
+
+
+		var oldWeights = readWeights();
+
+		// mutate best players
+		doMutations(winners, oldWeights);
+		
+		if (iterations >= minRemove) {
+			// remove worst player
+			var worstPlayer = players[players.length - 1];
+			console.log("Removing %s from population", worstPlayer);
+			// remove this player
+			delete oldWeights[worstPlayer];
+		}
+
+		saveWeights(oldWeights);
 	}
 }
 
